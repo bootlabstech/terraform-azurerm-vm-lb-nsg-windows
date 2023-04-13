b32701b23cbc59406e5a471dd0bb22135bd75e69
lalitha2407 <lalitha@bootlabstech.com>
2023-04-14 00:30:50 +0530

    inital

---
+++ main.tf
@@ -0,0 +1,171 @@
+resource "azurerm_windows_virtual_machine" "example" {
+  name                             = var.name
+  resource_group_name              = var.resource_group_name
+  location                         = var.location
+  size                             = var.size
+  admin_username                   = var.admin_username
+  admin_password                   = var.admin_password
+  network_interface_ids            = [azurerm_network_interface.network_interface.id]
+  license_type                     = var.license_type  
+
+  identity {
+    type = "SystemAssigned"
+  }
+  os_disk {
+    name                 = "${var.name}-disk"
+    caching              = "ReadWrite"
+    storage_account_type =  var.storage_account_type #"Standard_LRS"
+    disk_size_gb = var.disk_size_gb
+  }
+
+  source_image_reference {
+    publisher = var.publisher #MicrosoftWindowsServer:WindowsServer:2022-datacenter-azure-edition:20348.1006.220908
+    offer     = var.offer  #MicrosoftWindowsServer:WindowsServer:2019-datacenter-core-g2:17763.2686.220303
+    sku       = var.sku #MicrosoftWindowsServer:WindowsServer:2016-datacenter-gensecond:14393.5006.220305
+    version   = var.version
+  }
+   depends_on = [
+    azurerm_network_interface.network_interface
+  ]
+}
+# Creates Network Interface Card with private IP for Virtual Machine
+resource "azurerm_network_interface" "network_interface" {
+  name                = "{{.name}}-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  ip_configuration {
+    name                          = var.ip_name
+    subnet_id                     = var.subnet_id
+    private_ip_address_allocation = var.private_ip_address_allocation
+  }
+}
+# Creates Network Security Group NSG for Virtual Machine
+resource "azurerm_network_security_group" "nsg" {
+  name                = "{{.name}}-nsg"
+  location            = azurerm_windows_virtual_machine.example.location
+  resource_group_name = azurerm_windows_virtual_machine.example.resource_group_name
+}
+# Creates Network Security Group Default Rules for Virtual Machine
+
+resource "azurerm_network_security_rule" "nsg_rules" {
+  for_each                    = var.nsg_rules
+  name                        = each.value.name
+  priority                    = each.value.priority
+  direction                   = each.value.direction
+  access                      = each.value.access
+  protocol                    = each.value.protocol
+  source_address_prefix       = each.value.source_address_prefix
+  source_port_range           = each.value.source_port_range
+  destination_address_prefix  = each.value.destination_address_prefix
+  destination_port_range      = each.value.destination_port_range
+  network_security_group_name = azurerm_network_security_group.nsg.name
+  resource_group_name         = azurerm_windows_virtual_machine.example.resource_group_name
+}
+# Creates association (i.e) adds NSG to the NIC
+resource "azurerm_network_interface_security_group_association" "security_group_association" {
+  network_interface_id      = azurerm_network_interface.network_interface.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# Getting existing recovery_services_vault to add vm as a backup item 
+data "azurerm_recovery_services_vault" "services_vault" {
+  name                = var.recovery_services_vault_name
+  resource_group_name = var.services_vault_resource_group_name
+}
+# Getting existing Backup Policy for Virtual Machine
+data "azurerm_backup_policy_vm" "policy" {
+  name                = "{{.name}}-policy"
+  recovery_vault_name = data.azurerm_recovery_services_vault.services_vault.name
+  resource_group_name = data.azurerm_recovery_services_vault.services_vault.resource_group_name
+}
+# Creates Backup protected Virtual Machine
+resource "azurerm_backup_protected_vm" "backup_protected_vm" {
+  resource_group_name = var.resource_group_name
+  recovery_vault_name = data.azurerm_recovery_services_vault.services_vault.name
+  source_vm_id        = azurerm_windows_virtual_machine.example.id
+  backup_policy_id    = data.azurerm_backup_policy_vm.policy.id
+  depends_on = [
+    azurerm_virtual_machine.virtual_machine,
+    azurerm_backup_policy_vm.backup_policy_vm
+  ]
+}
+
+
+# Load Balancer
+
+resource "azurerm_public_ip" "public_ip" {
+  name                = "${var.name}-ip"
+  resource_group_name = azurerm_virtual_machine.vm.resource_group_name
+  location            = azurerm_virtual_machine.vm.location
+  ip_version          = var.ip_version
+  sku                 = var.public_ip_sku
+  sku_tier            = var.public_ip_sku_tier
+  allocation_method   = var.allocation_method
+  lifecycle {
+    ignore_changes = [
+      tags,
+    ]
+  }
+}
+
+resource "azurerm_lb" "lb" {
+  name                = "${var.name}-lb"
+  resource_group_name = azurerm_virtual_machine.vm.resource_group_name
+  location            = azurerm_virtual_machine.vm.location
+  sku                 = var.lb_sku
+  sku_tier            = var.lb_sku_tier
+  frontend_ip_configuration {
+    name                 = "${var.name}-pubIP"
+    public_ip_address_id = azurerm_public_ip.public_ip.id
+  }
+  depends_on = [
+    azurerm_public_ip.public_ip,
+    azurerm_virtual_machine.vm
+  ]
+  lifecycle {
+    ignore_changes = [
+      tags,
+    ]
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "backend_pool" {
+  name            = "${var.name}-backend_pool"
+  loadbalancer_id = azurerm_lb.lb.id
+  depends_on = [
+    azurerm_lb.lb
+  ]
+}
+
+
+# This resource block was attaching load balancer to vm 
+resource "azurerm_network_interface_backend_address_pool_association" "backend_association" {
+  network_interface_id    = azurerm_network_interface.nic.id
+  ip_configuration_name   = "${var.name}-ip"
+  backend_address_pool_id = azurerm_lb_backend_address_pool.backend_pool.id
+  depends_on = [
+    azurerm_network_interface.nic,
+    azurerm_lb_backend_address_pool.backend_pool
+  ]
+}
+
+
+resource "azurerm_lb_probe" "lb_probe" {
+  loadbalancer_id = azurerm_lb.lb.id
+  name            = "https"
+  port            = var.probe_ports
+
+}
+
+resource "azurerm_lb_rule" "lb_rule" {
+  loadbalancer_id                = azurerm_lb.lb.id
+  name                           = "htpps"
+  protocol                       = "Tcp"
+  frontend_port                  = 443
+  backend_port                   = 443
+  frontend_ip_configuration_name = "${var.name}-pubIP"
+  probe_id                       = azurerm_lb_probe.lb_probe.id
+  backend_address_pool_ids       = [azurerm_lb_backend_address_pool.backend_pool.id]
+}
+
+# UPDATE TAG: 
---
+++ variables.tf
@@ -0,0 +1,174 @@
+# virtual_machine
+variable "name" {
+  type        = string
+  description = "Specifies the name of the Virtual Machine. Changing this forces a new resource to be created."
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "name of the resource group"
+}
+
+variable "location" {
+  type        = string
+  description = "location of the resource group"
+}
+
+
+
+variable "size" {
+  type        = string
+  description = "Specifies the size of the Virtual Machine. See also Azure VM Naming Conventions."
+}
+
+variable "admin_username" {
+  type        = string
+  description = "Specifies the name of the local administrator account."
+}
+
+variable "admin_password" {
+  type        = string
+  description = "The password associated with the local administrator account."
+}
+variable "license_type" {
+  type        = string
+  description = "The pass"
+  
+}
+# os_disk
+variable "storage_account_type" {
+  type        = string
+  description = " Specifies the publisher of the image used to create the virtual machine. Examples: Canonical, MicrosoftWindowsServer"
+  default = "Standard_LRS"
+}
+
+variable "disk_size_gb" {
+  type        = string
+  description = "Specifies the offer of the image used to create the virtual machine. Examples: UbuntuServer, WindowsServer"
+  default = "WindowsServer"
+}
+
+# source_image_reference
+variable "publisher" {
+  type        = string
+  description = "Specifies the caching requirements for the Data Disk. Possible values include None, ReadOnly and ReadWrite."
+  default     = "MicrosoftWindowsServer"
+}
+
+variable "offer" {
+  type        = string
+  description = "Specifies how the data disk should be created. Possible values are Attach, FromImage and Empty."
+  default     = "WindowsServer"
+}
+
+variable "sku" {
+  type        = string
+  description = "Specifies the type of managed disk to create. Possible values are either Standard_LRS, StandardSSD_LRS, Premium_LRS or UltraSSD_LRS."
+
+}
+
+variable "version" {
+  type        = string
+  description = "Specifies the Operating System on the OS Disk. Possible values are Linux and Windows."
+
+}
+
+
+# azurerm_network_interface
+variable "ip_name" {
+  type        = string
+  description = "A name used for this IP Configuration."
+  default     = "internal"
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "The ID of the Subnet where this Network Interface should be located in."
+}
+
+variable "private_ip_address_allocation" {
+  type        = string
+  description = "The allocation method used for the Private IP Address. Possible values are Dynamic and Static"
+  default = "Dynamic"
+}
+# azurerm_network_security_rule
+variable "nsg_rules" {
+  type = map(object({
+    name                       = string
+    priority                   = number
+    direction                  = string
+    access                     = string
+    protocol                   = string
+    source_address_prefix      = string
+    source_port_range          = string
+    destination_address_prefix = string
+    destination_port_range     = string
+  }))
+  default = {
+    "https" = {
+      access                     = "Allow"
+      destination_address_prefix = "*"
+      destination_port_range     = "443"
+      direction                  = "Inbound"
+      name                       = "allow-https"
+      priority                   = 100
+      protocol                   = "Tcp"
+      source_address_prefix      = "*"
+      source_port_range          = "*"
+    }
+  }
+}
+# azurerm_recovery_services_vault
+variable "recovery_services_vault_name" {
+  type        = string
+  description = "name of the azurerm_network_security_group"
+}
+variable "services_vault_resource_group_name" {
+  type        = string
+  description = "name of the azurerm_network_security_group"
+}
+
+
+//load_balancer 
+
+variable "ip_version" {
+  type        = string
+  description = "The IP Version to use"
+  default     = "IPv4"
+}
+
+variable "public_ip_sku" {
+  description = "The SKU Tier that should be used for the Public IP. Possible values are Regional and Global"
+  type        = string
+  default     = "Standard"
+}
+
+variable "public_ip_sku_tier" {
+  description = "The SKU Tier that should be used for the Public IP. Possible values are Regional and Global, NOTE ---- When sku_tier is set to Global, sku must be set to Standard"
+  type        = string
+  default     = "Regional"
+}
+
+variable "allocation_method" {
+  type        = string
+  description = "Defines the allocation method for this IP address. Possible values are Static or Dynamic"
+  default     = "Static"
+}
+
+variable "lb_sku" {
+  type        = string
+  description = "The SKU of the Azure Load Balancer. Accepted values are Basic, Standard and Gateway. Defaults to Basic"
+  default     = "Standard"
+}
+
+variable "lb_sku_tier" {
+  type        = string
+  description = "The SKU tier of this Load Balancer. Possible values are Global and Regional. Defaults to Regional"
+  default     = "Regional"
+}
+
+variable "probe_ports" {
+  type        = number
+  description = "(optional) describe your variable"
+  default     = "443"
+}
