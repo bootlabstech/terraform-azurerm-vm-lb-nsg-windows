07a17f25915e735ad3795037ec9780c33abf4625
lalitha2407 <lalitha@bootlabstech.com>
2023-04-20 14:02:31 +0530

    dummy

--- main.tf
+++ main.tf
@@ -95,8 +95,8 @@ resource "azurerm_backup_protected_vm" "backup_protected_vm" {
 
 resource "azurerm_public_ip" "public_ip" {
   name                = "${var.name}-ip"
-  resource_group_name = azurerm_virtual_machine.vm.resource_group_name
-  location            = azurerm_virtual_machine.vm.location
+  resource_group_name = azurerm_windows_virtual_machine.example.resource_group_name
+  location            = azurerm_windows_virtual_machine.example.location
   ip_version          = var.ip_version
   sku                 = var.public_ip_sku
   sku_tier            = var.public_ip_sku_tier
@@ -110,8 +110,8 @@ resource "azurerm_public_ip" "public_ip" {
 
 resource "azurerm_lb" "lb" {
   name                = "${var.name}-lb"
-  resource_group_name = azurerm_virtual_machine.vm.resource_group_name
-  location            = azurerm_virtual_machine.vm.location
+  resource_group_name = azurerm_windows_virtual_machine.example.resource_group_name
+  location            = azurerm_windows_virtual_machine.example.location
   sku                 = var.lb_sku
   sku_tier            = var.lb_sku_tier
   frontend_ip_configuration {
@@ -140,7 +140,7 @@ resource "azurerm_lb_backend_address_pool" "backend_pool" {
 
 # This resource block was attaching load balancer to vm 
 resource "azurerm_network_interface_backend_address_pool_association" "backend_association" {
-  network_interface_id    = azurerm_network_interface.nic.id
+  network_interface_id    = azurerm_network_interface.network_interface.id
   ip_configuration_name   = "${var.name}-ip"
   backend_address_pool_id = azurerm_lb_backend_address_pool.backend_pool.id
   depends_on = [
