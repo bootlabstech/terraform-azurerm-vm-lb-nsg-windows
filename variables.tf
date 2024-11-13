f2bf889d4847aebee32252996bbad68d3dd750b1
lalita2407 <lalitalallu02@gmail.com>
2024-11-13 17:24:55 +0530

    removed public IP and variablised the ip id variable and chaged source ip prefixes in nsg rules for migration purposes

--- variables.tf
+++ variables.tf
@@ -96,7 +96,7 @@ variable "nsg_rules" {
     direction                  = string
     access                     = string
     protocol                   = string
-    source_address_prefix      = string
+    source_address_prefixes    = list(string)
     source_port_range          = string
     destination_address_prefix = string
     destination_port_range     = string
@@ -110,7 +110,7 @@ variable "nsg_rules" {
       name                       = "allow-https"
       priority                   = 100
       protocol                   = "Tcp"
-      source_address_prefix      = "*"
+      source_address_prefixes      = ["*"]
       source_port_range          = "*"
     }
   }
