1410b11a67f4eeb2ee97290fc0591c69b667933c
lalita2407 <lalitalallu02@gmail.com>
2024-12-31 10:27:51 +0530

    s1 agent

--- variables.tf
+++ variables.tf
@@ -43,31 +43,31 @@ variable "disk_size_gb" {
 
 }
 
-# source_image_reference
-variable "publisher" {
-  type        = string
-  description = "Specifies the Publisher of the Marketplace Image this Virtual Machine should be created from.View documentation for all options"
-  default     = "MicrosoftWindowsServer"
-}
+# # source_image_reference
+# variable "publisher" {
+#   type        = string
+#   description = "Specifies the Publisher of the Marketplace Image this Virtual Machine should be created from.View documentation for all options"
+#   default     = "MicrosoftWindowsServer"
+# }
 
-variable "offer" {
-  type        = string
-  description = " Specifies the offer of the image used to create the virtual machines.View documentation for all options "
-  default     = "WindowsServer"
-}
+# variable "offer" {
+#   type        = string
+#   description = " Specifies the offer of the image used to create the virtual machines.View documentation for all options "
+#   default     = "WindowsServer"
+# }
 
-variable "sku" {
-  type        = string
-  description = "Specifies the SKU of the image used to create the virtual machines.View documentation for all options"
+# variable "sku" {
+#   type        = string
+#   description = "Specifies the SKU of the image used to create the virtual machines.View documentation for all options"
 
-}
+# }
 
-variable "storage_image_version" {
-  type        = string
-  description = "Specifies the Operating System version on the OS Disk. View documentation for all options"
-  default = "latest"
+# variable "storage_image_version" {
+#   type        = string
+#   description = "Specifies the Operating System version on the OS Disk. View documentation for all options"
+#   default = "latest"
 
-}
+# }
 
 
 # azurerm_network_interface
