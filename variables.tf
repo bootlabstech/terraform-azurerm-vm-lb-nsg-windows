cc7d2074a8765f45406f2818bb3601b6385c462e
Lalitha <lalitha@LalitaLallu.local>
2025-04-24 16:43:22 +0530

    os sku with enchanced backup

--- variables.tf
+++ variables.tf
@@ -43,31 +43,28 @@ variable "disk_size_gb" {
 
 }
 
-# # source_image_reference
-# variable "publisher" {
-#   type        = string
-#   description = "Specifies the Publisher of the Marketplace Image this Virtual Machine should be created from.View documentation for all options"
-#   default     = "MicrosoftWindowsServer"
-# }
+ # source_image_reference
+ variable "publisher" {
+   type        = string
+   description = "Specifies the Publisher of the Marketplace Image this Virtual Machine should be created from.View documentation for all options"
+   default     = "MicrosoftWindowsServer"
+ }
+ variable "offer" {
+   type        = string
+   description = " Specifies the offer of the image used to create the virtual machines.View documentation for all options "
+   default     = "WindowsServer"
+ }
+ variable "sku" {
+   type        = string
+   description = "Specifies the SKU of the image used to create the virtual machines.View documentation for all options"
+ }
 
-# variable "offer" {
-#   type        = string
-#   description = " Specifies the offer of the image used to create the virtual machines.View documentation for all options "
-#   default     = "WindowsServer"
-# }
+ variable "storage_image_version" {
+   type        = string
+   description = "Specifies the Operating System version on the OS Disk. View documentation for all options"
+  default = "latest"
 
-# variable "sku" {
-#   type        = string
-#   description = "Specifies the SKU of the image used to create the virtual machines.View documentation for all options"
-
-# }
-
-# variable "storage_image_version" {
-#   type        = string
-#   description = "Specifies the Operating System version on the OS Disk. View documentation for all options"
-#   default = "latest"
-
-# }
+}
 
 
 # azurerm_network_interface
@@ -196,7 +193,3 @@ variable "keyvault_name" {
 #   type = string
   
 # }
-variable "image_id" {
-  type = string
-  
-}
